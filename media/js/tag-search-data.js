1857b53418f169c3c843034920a880a6ed1c723a
teckoo <c2.programmer@gmail.com>
2021-02-17 21:44:24 -0500

    add tag search

---
+++ media/js/tag-search-data.js
@@ -0,0 +1,47 @@
+var problems = {
+    "1": {
+        name: "1. Two Sum",
+        tags: ["array"],
+        keywords: ["two sum"],
+        url: "https://github.com/somelink",
+        notes: "quick note",
+        templates: ["two pointers"],
+        companies: ["bloomberg", "amazon"],
+    },
+    "2": {
+        name: "2",
+        tags: ["array", "string"],
+        keywords: ["change coin"],
+        url: "some link",
+        notes: "quick note",
+    },
+    "3": {
+        name: "3",
+        tags: ["array", "string", "tree"],
+        keywords: ["N queens"],
+        url: "some link",
+        notes: "quick note",
+    },
+    "4": {
+        name: "4",
+        tags: ["string", "tree"],
+        keywords: ["two sum"],
+        url: "some link",
+        notes: "quick note",
+    },
+    "5": {
+        name: "5",
+        tags: ["tree"],
+        keywords: ["two sum"],
+        url: "some link",
+        notes: "quick note",
+    },
+    "560": {
+        name: "560. subarray sum equals k",
+        tags: ["array", "hashmap", "prefix_sum"],
+        keywords: ["subarray", "sum"],
+        url: "https://leetcode.com/problems/subarray-sum-equals-k/",
+        notes: "get prefix sum for array, use hashamp as two-sum searching presum[i]-k time: O(N)",
+    },
+};
+
